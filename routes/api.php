001229f1ec070e2cb8445ee7ef9d9aa984d1239c
bilal-alrihani <en.belal.alrihani.1990.en@gmail.com>
2023-10-29 13:36:34 +0300

    solve api route problems

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('projects', ProjectController::class);
 Route::resource('tags', TagController::class);
 Route::resource('tasks', TaskController::class);
